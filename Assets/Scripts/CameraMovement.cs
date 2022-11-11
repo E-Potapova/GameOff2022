a6f7f0426f0cbcac266a7c6808423138dee89c6b
ZacharyFredeman <70460310+ZacharyFredeman@users.noreply.github.com>
2022-11-10 20:48:26 -0500

    Camera movement

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -5,31 +5,56 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
     //camera transform
-    Transform cameraTransform;
+    public Transform cameraTransform;
 
     //camera component
-    Camera cameraP;
+    //Camera cameraP;
 
     //camera cordinates
     float cameraX = 0;
-    float cameraY =0;
-    float cameraZ =0;
+    float cameraY = 0;
+    float cameraZ = 0;
+
+    //restrict camera movement bounds
+    public float minX;
+    public float maxX;
+
+    public float minY;
+    public float maxY;
+
+    public float moveSpeed =1;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        cameraTransform = GetComponent<Transform>();
-        cameraP = GetComponent<Camera>();
+        //cameraTransform = GetComponent<Transform>();
+        //cameraP = GetComponent<Camera>();
 
-        Debug.Log(cameraP);
-        Debug.Log(cameraTransform);
-        
+        //Debug.Log(cameraP);
+        //Debug.Log(cameraTransform);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //horizontial camera movement
+        float hMove = Input.GetAxis("Horizontal");
+        Vector3 mp = Vector3.zero;
+        mp.x = hMove * moveSpeed;
+
+        Vector3 targetPosition = cameraTransform.position;
+        //vertical camera movement
+        float vMove = Input.GetAxis("Vertical");
+        mp.y = vMove * moveSpeed;
+
+        targetPosition += mp;
+
+        targetPosition.x = Mathf.Clamp(targetPosition.x, minX, maxX + 0.01f);
+
+        targetPosition.y = Mathf.Clamp(targetPosition.y, minY, maxY + 0.01f);
+
+        cameraTransform.position = targetPosition;
+
     }
 }
