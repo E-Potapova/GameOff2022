c0ed622b4c70ee11fb46ad81b559bb3c5403439d
ZacharyFredeman <70460310+ZacharyFredeman@users.noreply.github.com>
2022-11-20 22:01:38 -0500

    Working on build mechanic

--- Assets/Scripts/CatManager.cs
+++ Assets/Scripts/CatManager.cs
@@ -75,6 +75,6 @@ public class CatManager : MonoBehaviour
 
     public enum Ability{
         //enter the abilities here
-        defaultWalk, stopper, umbrella, digFoward, digDown, dead,
+        defaultWalk, stopper, umbrella, digFoward, digDown, dead, builder, filler
     }
 }
