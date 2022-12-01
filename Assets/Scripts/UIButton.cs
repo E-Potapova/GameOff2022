84dedb29f6ddaadd4726d1e371bb05e17afd34d9
Elizabeth Potapova <78442201+E-Potapova@users.noreply.github.com>
2022-12-01 10:18:35 -0500

    bouncy UI

--- Assets/Scripts/UIButton.cs
+++ Assets/Scripts/UIButton.cs
@@ -7,23 +7,29 @@ using static CatManager;
 public class UIButton : MonoBehaviour
 {
     public Ability ability;
+    public Sprite defaultSprite;
+    public Sprite selectedSprite;
 
-    //private Animator animator;
+    private Animator animator;
+    private Image buttonImage;
 
-    //private void Start()
-    //{
-    //    animator = GetComponent<Animator>();
-    //}
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        buttonImage = GetComponent<Image>();
+    }
 
     public void Press(){
         //Debug.Log("onClick");
-        //animator.SetBool("selected", true);
+        animator.SetBool("selected", true);
+        buttonImage.sprite = selectedSprite;
         UIManager.singleton.PressAbilityButton(this);
     }
 
-    //public void Deselect()
-    //{
-    //    Debug.Log("deselect");
-    //    animator.SetBool("selected", false);
-    //}
+    public void Deselect()
+    {
+        Debug.Log("deselect");
+        animator.SetBool("selected", false);
+        buttonImage.sprite = defaultSprite;
+    }
 }
